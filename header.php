489b492009e98e62613512613fe11759343aaac6
timothee <tim.dorand@gmail.com>
2015-11-09 09:05:07 +0100

    commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+ * Date: 09/11/15
+ * Time: 08:49
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>NetFriends</title>
+    <meta charset="utf-8">
+
+    <link href="style.css">
+
+</head>
+<body>
+
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,3 +5,7 @@
  * Date: 09/11/15
  * Time: 08:45
  */
+
+include('header.php');
+
+echo 'Hello world';
